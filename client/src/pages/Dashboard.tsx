1a20c89e0cdea96c2f9fa32a6645fc3c4dc3e6b6
a-champi <a.champi.h@gmail.com>
2025-07-29 17:27:38 +0200

    added apis-render

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -36,13 +36,10 @@ const Dashboard = ({ onLogout }: Props) => {
   //HandleLogout
   const handleLogout = async () => {
     try {
-      const res = await fetch(
-        `${import.meta.env.VITE_API_URL}/api/auth/logout`,
-        {
-          method: "POST",
-          credentials: "include",
-        }
-      );
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/logout`, {
+        method: "POST",
+        credentials: "include",
+      });
       if (!res.ok) {
         throw new Error("Nicht eingeloggt");
       }
